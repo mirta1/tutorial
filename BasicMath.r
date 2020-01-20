70e7df935c7a40dc2a8c0a3b4925d01079f3ed81
mirta1 <oblakmirta@gmail.com>
2020-01-20 20:43:47 +0100

    20/01

---
+++ BasicMath.r
@@ -0,0 +1,37 @@
+1+1
+1+2+3
+7-5
+8+9-6
+
+
+3*4
+4*3*2
+
+4/2
+4/3
+
+PEMDAS
+
+4*6 + 5
+4*(6+5)
+
+X<-2
+X
+Y=5
+Y
+
+Z <-3
+Z
+a <-b <-7
+a
+b
+
+assign("j",4)
+j
+
+rm(j)
+j
+
+theVariable<-17
+theVariable
+THEVARIABLE
